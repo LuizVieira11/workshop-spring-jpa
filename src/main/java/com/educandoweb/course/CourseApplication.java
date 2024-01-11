2154bc85ea9d76bb2b1277b1f7c12df9967b9bda
Luiz Gustavo <luiz.vieira11@outlook.com>
2024-01-11 16:32:51 -0300

    Swagger documentation

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,9 +1,14 @@
 package com.educandoweb.course;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "WebServices project with Spring Boot and JPA/Hibernate", version = "1.0",
+		description = "Created to use database concept and connections in an REST API"))
 public class CourseApplication {
 
 	public static void main(String[] args) {
